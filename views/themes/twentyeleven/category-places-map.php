30f2e82b39f1af70337cf9ef2f00eaa186b00fe0
Clay Graham <claytantor@gmail.com>
2012-01-13 22:40:33 -0800

    testing new post commit hook
    testing new post commit hook

--- views/themes/twentyeleven/category-places-map.php
+++ views/themes/twentyeleven/category-places-map.php
@@ -18,7 +18,7 @@ global $wp_query;
 $wlecCatObject = get_category( $wp_query->query_vars['cat'])
 ?>
 <?php get_header(); ?>
-<!-- include -->
+<!-- include for me -->
 <?php 
 
 $places_list_include = WP_PLUGIN_DIR . '/' .$wlPlaces->pluginDir . '/views/includes/category-map-include.php';
