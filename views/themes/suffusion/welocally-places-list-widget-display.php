a70636911c589b79381b990b4bbb645d5d4c61a0
Jorge A. Torres <jtorresh@gmail.com>
2012-02-07 17:16:13 -0500

    Bug #70 - update list and map widget to support new tags paradigm

--- views/themes/suffusion/welocally-places-list-widget-display.php
+++ views/themes/suffusion/welocally-places-list-widget-display.php
@@ -6,7 +6,10 @@ $index = 0;
 <div class="widget sidebar-item dbx-box suf-widget">
 <div>
 <h3 style="margin-left:5px;"><?php echo $title; ?></h3>
-<?php foreach( $posts as $post ) { ?>
+<?php
+foreach ($posts as $post):
+    foreach (get_post_places($post) as $place):
+?>
 
 <?php /**
  * This is the template for the output of the events list widget. 
@@ -14,7 +17,7 @@ $index = 0;
  * There is currently no default styling, which is highly needed.
  * @return string
  */
-$PlaceSelected	= get_post_meta( $post->ID, '_PlaceSelected', true );
+$PlaceSelected	= $place;
 
 if( $PlaceSelected != '' && $_REQUEST['json'] != 'get_post') : 
 
@@ -23,13 +26,14 @@ include($places_list_include);
 ?>
 
 <div style="margin-left:5px; margin-right:5px;">
-<div class="wl-place-widget-category" id="plugin-place-category<?php echo $post->ID; ?>" style="{display:none;}"><?php echo $this->get_categories($post->ID, $exclude_cats); ?></div>
-<div id="plugin-place<?php echo $post->ID; ?>"></div>
+<div class="wl-place-widget-category" id="plugin-place-category<?php echo $index; ?>" style="{display:none;}"><?php echo $this->get_categories($post->ID, $exclude_cats); ?></div>
+<div id="plugin-place<?php echo $index; ?>"></div>
 </div>
 <?php endif; ?>
 <?php
 	$index=$index+1;
-}
+	endforeach;
+endforeach;
 ?>
 </div>
 </div>
