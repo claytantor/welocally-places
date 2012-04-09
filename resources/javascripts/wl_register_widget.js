d4377536bd2ff86cdea216ae946bb7c3618e579d
Clay Graham <claytantor@gmail.com>
2012-04-08 20:08:10 -0700

    that should not have required that

--- resources/javascripts/wl_register_widget.js
+++ resources/javascripts/wl_register_widget.js
@@ -352,7 +352,7 @@ WELOCALLY_RegisterWidget.prototype.registrationHandler = function(event, ui) {
 WELOCALLY_RegisterWidget.prototype.editHandler = function(event, ui) {
 	var _instance = event.data.instance;
 	 
-	//copy all field area to values 
+	//copy all field area to values weird
 	jQuery('#wl_register_value_site_name')
 		.html(jQuery('#wl_register_site_name').val());	
 	
